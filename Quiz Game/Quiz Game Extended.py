88954b8862f979518328b7fe69421693fd8075ee
Rugsoft <futuroelectronico@gmail.com>
2025-09-04 20:31:46 +0200

    implementado el quiz game

--- Quiz Game/Quiz Game Extended.py
+++ Quiz Game/Quiz Game Extended.py
@@ -60,14 +60,46 @@ def solicitar_datos():
             print("Valores incorrectos, vuelve a rellenarlo")
 
 def quiz_game(datos):
-    pass
+    puntuacion = 0
+    
+    for i, dato in enumerate(datos):
+        pregunta = html.unescape(dato["question"])
+        respuesta_correcta = html.unescape(dato["correct_answer"])
+        respuestas_incorrectas = [html.unescape(r) for r in dato["incorrect_answers"]]
+        
+        opciones = respuestas_incorrectas + [respuesta_correcta]
+        random.shuffle(opciones)
+        
+        print(f"\n{i+1}- {pregunta}")
+        for n, opcion in enumerate(opciones):
+            print(f"    {n+1}. {opcion}")
+        
+        while True:
+            try:
+                respuesta_usuario = int(input("Introduce el numero de la respuesta: "))
+                if respuesta_usuario >= 1 and respuesta_usuario <= len(opciones):
+                    break
+                else:
+                    print("Introduce un numero válido!")
+            except ValueError:
+                print("Introduce un numero!")
+        
+        respuesta_usuario = opciones[respuesta_usuario - 1]
+        
+        if respuesta_usuario == respuesta_correcta:
+            puntuacion += 1
+            print("Respuesta correcta")
+        else:
+            print(f"Respuesta incorrecta, la correcta era: {respuesta_correcta}")
+    
+    print(f"Juegos Finalizado!! Has sacado un {puntuacion}/{len(datos)}")
     
   
 def main():
-    print("                        =====  QUIZ GAME  =====")
+    print("\n                        =====  QUIZ GAME  =====")
     cantidad, categoria, dificultad = solicitar_datos()
     preguntas = solicitar_preguntas(cantidad, categoria, dificultad)
-    print(preguntas)
+    quiz_game(preguntas)
 
 
 if __name__ == "__main__":
